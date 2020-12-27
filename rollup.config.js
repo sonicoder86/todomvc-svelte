5cb858b387296e474accff636d9d85e719e4624e
blacksonic <soos.gabor86@gmail.com>
2020-12-27 14:18:54 +0100

    upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -23,12 +23,14 @@ export default {
       minimize: production
 		}),
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
+				// we'll extract any component CSS out into
+				// a separate file - better for performance
+				css: css => {
+					css.write('public/build/bundle.css');
+				}
 			}
 		}),
 
