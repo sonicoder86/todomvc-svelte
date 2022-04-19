dc63ca5f6d8bf35e56b12c1d53ec5af8084f45a8
Gabor Soos <soos.gabor86@gmail.com>
2022-04-19 19:51:51 +0200

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -9,59 +9,59 @@ import { terser } from 'rollup-plugin-terser';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
+  },
+  plugins: [
     // css({ output: 'public/build/extra.css' }),
-		postCss({
-      extract: 'public/build/extra.css',
+    postCss({
+      extract: 'extra.css',
       minimize: production
-		}),
-		svelte({
-			compilerOptions: {
-				// enable run-time checks when not in production
-				dev: !production,
-				// we'll extract any component CSS out into
-				// a separate file - better for performance
-				css: css => {
-					css.write('public/build/bundle.css');
-				}
-			}
-		}),
+    }),
+    svelte({
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        css: css => {
+          css.write('bundle.css');
+        }
+      }
+    }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve({
-			contentBase: 'public',
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve({
+      contentBase: 'public',
       host: 'localhost',
       port: 9500
-		}),
+    }),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload('public'),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
